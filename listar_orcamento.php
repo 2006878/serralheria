f203e48d977c54d28682f434a6b882d2f746788e
TAIRONE LEANDRO DO AMARAL <taironeamaral@bol.com.br>
2021-03-02 08:09:12 -0300

    Retirada do campo email da visualização
    Retirada do campo email da visualização

--- listar_orcamento.php
+++ listar_orcamento.php
@@ -11,10 +11,10 @@ if(($resultado_orcamento) AND ($resultado_orcamento->num_rows != 0)){
 			<tr>
 				<th>ID</th>
 				<th>Nome</th>
-                <th>E-mail</th>
-                <th>Telefone</th>
-                <th>Endereço</th>
-                <th>Descrição</th>
+                		<!--th>E-mail</th-->
+                		<th>Telefone</th>
+                		<th>Endereço</th>
+                		<th>Descrição</th>
 			</tr>
 		</thead>
 		<tbody>
@@ -22,13 +22,13 @@ if(($resultado_orcamento) AND ($resultado_orcamento->num_rows != 0)){
             while($row_orcamento =mysqli_fetch_assoc($resultado_orcamento)){
                 ?>
                 <tr>
-					<th><?php echo $row_orcamento['id']; ?></th>
-					<td><?php echo $row_orcamento['nome']; ?></td>
-					<td><?php echo $row_orcamento['email']; ?></td>
-                    <th><?php echo $row_orcamento['telefone']; ?></th>
-					<td><?php echo $row_orcamento['endereco']; ?></td>
-					<td><?php echo $row_orcamento['descricao']; ?></td>
-				</tr>
+			<th><?php echo $row_orcamento['id']; ?></th>
+			<td><?php echo $row_orcamento['nome']; ?></td>
+			<!--td><?php echo $row_orcamento['email']; ?></td-->
+                    	<th><?php echo $row_orcamento['telefone']; ?></th>
+			<td><?php echo $row_orcamento['endereco']; ?></td>
+			<td><?php echo $row_orcamento['descricao']; ?></td>
+		</tr>
                 <?php
 			}?>
 		</tbody>
